8130001c37b92acd7e495237b2cf0cce67b741ce
natashamwafulirwa <natasha.mwafulirwa@nivohub.com>
2023-03-20 23:05:42 +0000

    Set up Hero JS and added header.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen
+    mx-auto'>
+      <div className={`${styles.paddingX} absolute
+      inset-0 top-[120px] max-w-7xl mx-auto flex
+      flex-row items-start gap-5`}>
+      <div className="flex flex-col
+      justify-center items-center mt-5">
+
+<div>
+  <h1 className={`${styles.heroHeadText}
+  text-white`}>Hi, I'm Tasha</h1>
+  <p className={`${styles.heroSubText} mt-2
+  text-white-100`}>
+  A Full-Stack Developer <br className="sm:block hidden" />
+  and Content Creator.
+  </p>
+</div>
+</div>
+</div>
+
+    </section>
   )
 }
 
